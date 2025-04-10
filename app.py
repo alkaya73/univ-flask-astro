fa35ff183bcd2640855b256c8dea0689c9efbe3c
alkaya73 <ali-han.kaya@etu.univ-smb.fr>
2025-04-10 11:41:01 +0000

    ETRS612

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, request 
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def home():
+    """HTML Index page"""
+    return render_template('index.html')
+
+@app.route("/inscription")
+def inscription():
+    """HTML inscription page"""
+    return render_template('inscription.html')
+
+@app.route("/connexion")
+def connexion():
+    """HTML connexion page"""
+    return render_template('connexion.html')
+
+@app.route("/appareils")
+def appareils():
+    """HTML appareil-photo page"""
+    return render_template('appareils.html')
+
+@app.route("/telescopes")
+def telescopes():
+    """HTML telescope page"""
+    return render_template('telescopes.html')
+
+@app.route("/photographies")
+def photos():
+    """HTML photo page"""
+    return render_template('photographies.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
